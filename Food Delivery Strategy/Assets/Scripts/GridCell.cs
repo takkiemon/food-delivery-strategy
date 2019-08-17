4e044fd5179e9c53b9fdcc876d92d7ca07156401
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-08-18 01:17:04 +0200

    add basic scripts: playercontroller, gamegrid, gridcell

---
+++ Food Delivery Strategy/Assets/Scripts/GridCell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    public int X;
+    public int Y;
+
+    GridCell()
+    {
+
+    }
+
+    GridCell(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Food Delivery Strategy/Assets/Scripts/MapGridBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGridBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateGrid(int width, int height)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                //create cell
+                //give the cell the coordinates
+            }
+        }
+    }
+}
