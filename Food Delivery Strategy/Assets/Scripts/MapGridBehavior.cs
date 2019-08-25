6fae8b2c31d57a3facc6cfa7b93f0951a423d7cd
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-08-26 00:55:03 +0200

    add support for different orientations of the gridmap (facing forward or facing upward / standing up or lying down)

--- Food Delivery Strategy/Assets/Scripts/MapGridBehavior.cs
+++ Food Delivery Strategy/Assets/Scripts/MapGridBehavior.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class MapGridBehavior : MonoBehaviour
 {
+    public GridCell[,] gridMapArray;
+    public int mapHeight;
+    public int mapWidth;
+    public float positionAmplifier;
+    public float minimumAmplifier;
+    public bool gridIsStanding;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DestroyGrid();
+        CreateGrid(mapWidth, mapHeight);
+        gridIsStanding = true;
     }
 
     // Update is called once per frame
@@ -18,12 +27,47 @@ public class MapGridBehavior : MonoBehaviour
 
     public void CreateGrid(int width, int height)
     {
-        for (int i = 0; i < height; i++)
+        gridMapArray = new GridCell[mapWidth, mapHeight];
+
+        for (int y = 0; y < height; y++)
         {
-            for (int j = 0; j < width; j++)
+            for (int x = 0; x < width; x++)
             {
-                //create cell
-                //give the cell the coordinates
+                if (!gridIsStanding)
+                {
+                    gridMapArray[x, y] = new GridCell(x, 0, y);
+                }
+                else
+                {
+                    gridMapArray[x, y] = new GridCell(x, y);
+                }
+            }
+        }
+    }
+
+    public void DestroyGrid()
+    {
+        gridMapArray = new GridCell[0, 0];
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 1);
+
+        if (positionAmplifier < minimumAmplifier)
+        {
+            positionAmplifier = minimumAmplifier;
+        }
+        if (gridMapArray != null && gridMapArray.Length > 0)
+        {
+            foreach (GridCell cell in gridMapArray)
+            {
+                Gizmos.DrawWireCube(
+                    new Vector3(cell.X * positionAmplifier,
+                                cell.Y * positionAmplifier,
+                                cell.Z * positionAmplifier),
+                    new Vector3(positionAmplifier, positionAmplifier, 0)
+                    );
             }
         }
     }
