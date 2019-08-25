7edfa0b4c9d6db93e5183ae2cd691d0518d7fcee
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-08-26 00:53:49 +0200

    add z-direction for cells

--- Food Delivery Strategy/Assets/Scripts/GridCell.cs
+++ Food Delivery Strategy/Assets/Scripts/GridCell.cs
@@ -2,31 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridCell : MonoBehaviour
+public class GridCell
 {
     public int X;
     public int Y;
+    public int Z;
 
-    GridCell()
+    public GridCell()
     {
-
+        X = -1;
+        Y = -1;
+        Z = 0;
     }
 
-    GridCell(int x, int y)
+    public GridCell(int x, int y)
     {
         X = x;
         Y = y;
+        Z = 0;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public GridCell(int x, int y, int z)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        X = x;
+        Y = y;
+        Z = z;
     }
 }
